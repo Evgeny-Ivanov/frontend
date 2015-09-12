eab0b20ba3b2afa507ea34eb397d78e753c59c1b
evgeny-ivanov <Saboteuur@yandex.ru>
2015-09-12 13:42:16 +0300

    Hello git

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        shell: {
+            options: {
+                stdout: true,
+                stderr: true
+            },
+            server: {
+                command: 'java -cp L1.2-1.0-jar-with-dependencies.jar main.Main 8080'
+            }
+        },
+        fest: {
+            templates: {
+                files: [{
+                    expand: true,
+                    cwd: 'templates', /* исходная директория */
+                    src: '*.xml', /* имена шаблонов */
+                    dest: 'public_html/js/tmpl' /* результирующая директория */
+                }],
+                options: {
+                    template: function (data) {/* формат функции-шаблона */
+                        return grunt.template.process(/* присваиваем функцию-шаблон переменной */
+                            'var <%= name %>Tmpl = <%= contents %> ;',
+                            {data: data}
+                        );
+                    }
+                }
+            }
+        },
+        watch: {
+            fest: {
+                files: ['templates/*.xml'],
+                tasks: ['fest'],
+                options: {
+                    interrupt: true,
+                    atBegin: true
+                }
+            },
+            server: {
+                files: [
+                    'public_html/js/**/*.js',
+                    'public_html/css/**/*.css'
+                ],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        concurrent: {
+            target: ['watch', 'shell'],
+            options: {
+                logConcurrentOutput: true /* Вывод процесса */
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-fest');
+
+    grunt.registerTask('default', ['concurrent']);
+
+};
