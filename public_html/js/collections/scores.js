cab474992dd74fc729647486662fe7653fa0e42d
evgeny-ivanov <Saboteuur@yandex.ru>
2015-09-22 13:55:41 +0300

    Коллекции доведены до ума

--- public_html/js/collections/scores.js
+++ public_html/js/collections/scores.js
@@ -11,6 +11,35 @@ define([
     	//компаратор применяется что бы постоянно поддерживать коллекцию в отсортированном состоянии
     });
 
-    //return new Collection();//не мое
-    return Collection;
+    //java-сервер ajax-ом должен сюда прислать 10 лучших пользователей
+    var collections = new Collection([
+                    new scoreModel({
+                        name: 'Женя',
+                        score: -77777
+                    }),
+                    new scoreModel({
+                        name: 'Том Круз',
+                        score: -43
+                    }),
+                    new scoreModel({
+                        name: 'Энди Дюфрейн',
+                        score: 453
+                    }),
+                    new scoreModel({
+                        name: 'Брюс Уэйн',
+                        score: -43
+                    }),      
+                    new scoreModel()           
+                ]);
+
+    collections = collections.sortBy(function(model) {
+        return -model.get('score');
+    });//collections - теперь просто массив 
+
+    collections = collections.slice(0,10);//не выдает ошибки если нет 10 элементов
+
+    collections = new Collection(collections);
+
+    return collections;
+
 });
--- public_html/js/views/scoreboard.js
+++ public_html/js/views/scoreboard.js
@@ -18,24 +18,10 @@ define([
             "click ":"hide",
         },
         initialize: function () {
-            var collections = new collectionsScores([
-                    this.model,
-                    new scoreModel({
-                        name: 'Женя',
-                        score: -77777
-                    }),
-                    new scoreModel({
-                        name: 'Том Круз',
-                        score: -43
-                    })                 
-                ]);
             this.$el.html(this.template());
-            var self = this;
-            collections.sortBy(function(model) {
-                return model.get('score');
-            });
-            //forEach и each
-            collections.each(function(num){
+
+            var self = this;             
+            collectionsScores.forEach(function(num){
                 self.$el.append('<h1>'+num.get('name')+' : '+num.get('score')+'</h1/><p>');
             });
         },
